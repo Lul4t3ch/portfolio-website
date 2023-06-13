503217dee0dd6a5288cbc13d76325e81b075fd0f
Lul4t3ch <lul4t3ch15@gmail.com>
2023-06-13 16:14:17 -0300

    alterações no front end da aplicação

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,11 @@
 import logo from "./images/logo.png";
 import banner from "./images/banner.png";
-import projectOne from "./images/projects/projectOne.jpg";
-import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import projectOne from "./images/projects/projectOne.png";
+import projectTwo from "./images/projects/projectTwo.png";
+import projectThree from "./images/projects/projectThree.png";
+import projectFour from "./images/projects/projectFour.png";
+import projectFive from "./images/projects/projectFive.png";
+import projectSix from "./images/projects/projectSix.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 
@@ -14,8 +15,9 @@ export {
   projectOne,
   projectTwo,
   projectThree,
-  testimonialOne,
-  testimonialTwo,
+  projectFour,
+  projectFive,
+  projectSix,
   quote,
   contactImg,
 };
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { projectOne, projectTwo, projectThree, projectFour, projectFive, projectSix } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -11,46 +11,58 @@ const Projects = () => {
     >
       <div className="flex justify-center items-center text-center">
         <Title
-          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
-          des="My Projects"
+          title="DÊ UMA OLHADA NO MEU PORTIFÓLIO E DEIXE SEU FEEDBACK"
+          des="Meus Projetos"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
+          title="ESCOLA API"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectOne}
+          repo="https://github.com/Lul4t3ch/escola-react-app"
+          site="http://35.199.111.146:82/"
         />
         <ProjectsCard
-          title="E-commerce Website"
+          title="PROJETO AGENDA"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectTwo}
+          repo="https://github.com/Lul4t3ch/projeto-agenda"
+          site="http://35.199.111.146"
         />
         <ProjectsCard
-          title="Chatting App"
+          title="API REST"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
           src={projectThree}
+          repo="https://github.com/Lul4t3ch/Api-Rest"
+          site="http://35.199.111.146:81/alunos"
         />
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
+          title="LANDING PAGE"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
+          src={projectFour}
+          repo="https://github.com/Lul4t3ch/responsive-landing-page"
+          site="https://lul4t3ch.github.io/responsive-landing-page/"
         />
         <ProjectsCard
-          title="E-commerce Website"
+          title="POKÉDEX"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
+          src={projectFive}
+          repo="https://github.com/Lul4t3ch/Pokedex"
+          site="#"
         />
         <ProjectsCard
-          title="Chatting App"
+          title="CLONE PLATAFORMA DIO"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
+          src={projectSix}
+          repo="https://github.com/Lul4t3ch/Clone-plataforma-DIO"
+          site="#"
         />
       </div>
     </section>

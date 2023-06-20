5f18dca9485292ca1ad83ca331c6ff6c1fb0a7b1
Lul4t3ch <lul4t3ch15@gmail.com>
2023-06-20 14:47:29 -0300

    alteraçoes

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -18,48 +18,49 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="ESCOLA API"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Aplicação Front end que consome uma Api Rest. 
+          Simulando o sistema web de uma escola, o site conta com as páginas de Login, 
+          Home, Edição de informações e cadastro."
           src={projectOne}
           repo="https://github.com/Lul4t3ch/escola-react-app"
           site="http://35.199.111.146:82/"
         />
         <ProjectsCard
           title="PROJETO AGENDA"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Agenda simples utilizando as quatro operações: Create (inserção),
+           Read (consulta), Update (atualização) e Delete (exclusão). "
           src={projectTwo}
           repo="https://github.com/Lul4t3ch/projeto-agenda"
           site="http://35.199.111.146"
         />
         <ProjectsCard
           title="API REST"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" API REST para consumo de dados. Fornece dados de usuários e alunos"
           src={projectThree}
           repo="https://github.com/Lul4t3ch/Api-Rest"
           site="http://35.199.111.146:81/alunos"
         />
         <ProjectsCard
           title="LANDING PAGE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Uma página simples e responsiva onde eu coloquei em prática todo o conhecimento de HTML,
+          CSS e JS adquirido durante meus estudos."
           src={projectFour}
           repo="https://github.com/Lul4t3ch/responsive-landing-page"
           site="https://lul4t3ch.github.io/responsive-landing-page/"
         />
         <ProjectsCard
           title="POKÉDEX"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Projeto muito divertido que apresenta todos os 151 pokémons da primeira
+           geração através do consumo de uma API REST!"
           src={projectFive}
           repo="https://github.com/Lul4t3ch/Pokedex"
           site="#"
         />
         <ProjectsCard
           title="CLONE PLATAFORMA DIO"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Clonagem da plataforma da Digital Innovation One, levando em 
+          consideração as principais páginas da plataforma, sendo elas: Home, 
+          Login, Cadastro e feed."
           src={projectSix}
           repo="https://github.com/Lul4t3ch/Clone-plataforma-DIO"
           site="#"
--- src/components/resume/Curriculo.js
+++ src/components/resume/Curriculo.js
@@ -9,7 +9,7 @@ const Curriculo = () => {
 
       <div className="w-full lgl:w-1/2 flex justify-center items-center gap-20">
       <button className="w-1/2 h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent">
-        <b class="text-white"> Baixar Curriculo</b> 
+        <b class="text-white">Baixar Curriculo</b> 
       </button>
     </div>
     
